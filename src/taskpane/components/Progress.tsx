ab26e0befeaf1d87ee321ce043a24edd6e5cd4e8
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2019-08-29 16:41:58 +0200

    Simplify process loader

--- src/taskpane/components/Progress.tsx
+++ src/taskpane/components/Progress.tsx
@@ -1,26 +1,14 @@
-import * as React from 'react';
-import { Spinner, SpinnerType } from 'office-ui-fabric-react';
+import * as React from 'react'
+import { Spinner, SpinnerType } from 'office-ui-fabric-react'
 
 export interface ProgressProps {
-    logo: string;
-    message: string;
-    title: string;
+  message: string
 }
 
 export default class Progress extends React.Component<ProgressProps> {
-    render() {
-        const {
-            logo,
-            message,
-            title,
-        } = this.props;
+  render() {
+    const { message } = this.props
 
-        return (
-            <section className='ms-welcome__progress ms-u-fadeIn500'>
-                <img width='90' height='90' src={logo} alt={title} title={title} />
-                <h1 className='ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary'>{title}</h1>
-                <Spinner type={SpinnerType.large} label={message} />
-            </section>
-        );
-    }
+    return <Spinner type={SpinnerType.large} label={message} style={{ marginTop: '20px' }} />
+  }
 }
