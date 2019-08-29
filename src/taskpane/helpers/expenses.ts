1c1ed87b0b07cffe61589da70cdd3438e9154700
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2019-08-29 15:15:05 +0200

    Add dummy data

---
+++ src/taskpane/helpers/expenses.ts
@@ -0,0 +1,51 @@
+type Expense = {
+  merchant: string
+  employee: string
+  amount: {
+    value: number
+    currency: string
+  }
+}
+
+export const ExpenseList: Expense[] = [
+  {
+    merchant: 'Walmart',
+    employee: 'Zoey',
+    amount: {
+      value: 53,
+      currency: 'DKK',
+    },
+  },
+  {
+    merchant: 'Stick N Sushi',
+    employee: 'Aimee',
+    amount: {
+      value: 349,
+      currency: 'DKK',
+    },
+  },
+  {
+    merchant: 'Boston Tools',
+    employee: 'Ian',
+    amount: {
+      value: 83,
+      currency: 'USD',
+    },
+  },
+  {
+    merchant: 'Netto',
+    employee: 'Zoey',
+    amount: {
+      value: 3,
+      currency: 'EUR',
+    },
+  },
+  {
+    merchant: 'Tacos Locos',
+    employee: 'Diego',
+    amount: {
+      value: 100,
+      currency: 'DKK',
+    },
+  },
+]
