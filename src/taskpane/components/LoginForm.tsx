8b36673bfcbc96c297ef340d9335a06b9e21e1a7
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2019-08-29 15:14:44 +0200

    Add login form

---
+++ src/taskpane/components/LoginForm.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import { Button, ButtonType, TextField, Stack } from 'office-ui-fabric-react'
+
+interface Props {
+  email: string
+  password: string
+  inputHandler: (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue?: string
+  ) => void
+  formHandler: () => void
+}
+
+export default class LoginForm extends React.Component<Props> {
+  render() {
+    const { email, password, inputHandler, formHandler } = this.props
+    return (
+      <Stack tokens={{ childrenGap: 10, padding: 20 }}>
+        <TextField
+          label="Your Email"
+          iconProps={{ iconName: 'Mail' }}
+          value={email}
+          name="email"
+          onChange={inputHandler}
+        />
+        <TextField
+          label="Your password"
+          iconProps={{ iconName: 'Lock' }}
+          value={password}
+          name="password"
+          onChange={inputHandler}
+        />
+
+        <Button
+          className="ms-welcome__action"
+          buttonType={ButtonType.default}
+          onClick={formHandler}
+          iconProps={{ iconName: 'ChevronRight' }}
+          style={{ borderRadius: '22px', marginTop: '40px' }}
+          disabled={!email || !password}
+        >
+          Login
+        </Button>
+      </Stack>
+    )
+  }
+}
