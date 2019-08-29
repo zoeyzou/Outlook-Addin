c369f1aec9c724fca8154fc7a1148b96a263480d
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2019-08-29 17:56:22 +0200

    Add list and details

---
+++ src/taskpane/components/ExpenseDetails.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import { teamOptions, categoryOptions } from '../helpers/expenses'
+import { Stack, Dropdown, Icon, TextField, IconButton } from 'office-ui-fabric-react'
+
+interface Props {}
+
+export default class ExpenseDetails extends React.Component<Props> {
+  render() {
+    return (
+      <Stack tokens={{ childrenGap: 10, padding: 20 }}>
+        <Stack.Item styles={{ root: { display: 'flex', width: '100%' } }}>
+          <Icon
+            iconName="People"
+            title="People"
+            styles={{ root: { fontSize: '20px', margin: '0 5px' } }}
+          />
+          <Dropdown options={teamOptions} styles={{ root: { width: '100%' } }} />
+        </Stack.Item>
+        <Stack.Item styles={{ root: { display: 'flex', width: '100%' } }}>
+          <Icon
+            iconName="AllApps"
+            title="Category"
+            styles={{ root: { fontSize: '20px', margin: '0 5px' } }}
+          />
+          <Dropdown options={categoryOptions} styles={{ root: { width: '100%' } }} />
+        </Stack.Item>
+        <Stack.Item styles={{ root: { display: 'flex', width: '100%' } }}>
+          <Icon
+            iconName="Handwriting"
+            title="Notes"
+            styles={{ root: { fontSize: '20px', margin: '0 5px' } }}
+          />
+          <TextField multiline styles={{ root: { width: '100%' } }} />
+        </Stack.Item>
+        <Stack.Item styles={{ root: { display: 'flex', width: '100%' } }}>
+          <Icon
+            iconName="Attach"
+            title="File"
+            styles={{ root: { fontSize: '20px', margin: '0 5px' } }}
+          />
+          <IconButton
+            iconProps={{ iconName: 'Add', styles: { root: { fontSize: '30px' } } }}
+            styles={{ root: { width: '100%', height: '80px', border: '1px dashed' } }}
+          />
+        </Stack.Item>
+      </Stack>
+    )
+  }
+}
--- src/taskpane/components/ExpenseList.tsx
+++ src/taskpane/components/ExpenseList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Expense } from '../helpers/expenses'
 import { List } from 'office-ui-fabric-react'
+import ExpenselistItem from './ExpenseListItem'
 
 interface Props {
   expenseList?: Expense[]
@@ -13,44 +14,13 @@ export default class ExpenseList extends React.Component<Props> {
       return <h4 style={{ padding: '20px' }}>"Great, there's no expense that misses Receipt!"</h4>
     }
 
-    return <List items={expenseList} onRenderCell={this.getExpenseItem} />
-  }
-
-  private getExpenseItem = (expense: Expense) => {
     return (
-      <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px' }}>
-        <div style={{ display: 'flex', flexFlow: 'row', alignItems: 'flex-start' }}>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: '40px',
-              height: '40px',
-              marginRight: '15px',
-              borderRadius: '20px',
-              color: 'white',
-              backgroundColor: getRandomColor(),
-            }}
-          >
-            {expense.merchant[0]}
-          </div>
-          <div style={{ display: 'flex', flexFlow: 'column' }}>
-            <h5>{expense.merchant}</h5>
-            <p>{expense.employee}</p>
-          </div>
-        </div>
-        <div>
-          {expense.amount.value.toFixed(2)} {expense.amount.currency}
-        </div>
-      </div>
+      <List
+        items={expenseList}
+        onRenderCell={(expense: Expense, index: number) => (
+          <ExpenselistItem expense={expense} index={index} />
+        )}
+      />
     )
   }
-}
-
-function getRandomColor() {
-  const h = Math.floor(Math.random() * 360)
-  const s = Math.floor(Math.random() * 100)
-  const l = Math.floor(Math.random() * 100)
-  return `hsl(${h}, ${s}%, ${l}%)`
 }
